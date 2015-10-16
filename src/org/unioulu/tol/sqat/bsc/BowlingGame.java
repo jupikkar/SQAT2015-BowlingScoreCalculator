4a69be3d6028a9cb2947b2a7b705aecb35de2cf3
somename <someemail>
2015-10-16 13:23:54 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/BowlingGame.java
+++ src/org/unioulu/tol/sqat/bsc/BowlingGame.java
@@ -26,7 +26,7 @@ public class BowlingGame {
 		for (Frame frame : frames){
 			
 			if (wasStrike && frame.isStrike())
-				gameScore += (2 * frame.score()) + frame.getFirstThrow();
+				gameScore += frame.score() + frame.score() + frame.getFirstThrow();
 			else if (wasStrike)
 				gameScore += 2 * frame.score();
 			else if (wasSpare)

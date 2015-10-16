f108714b616b211bc42ae206cd2657d7132be952
somename <someemail>
2015-10-16 12:18:47 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/Frame.java
+++ src/org/unioulu/tol/sqat/bsc/Frame.java
@@ -16,8 +16,10 @@ public class Frame {
 
 	//returns whether the frame is a strike or not
 	public boolean isStrike(){
-		//to be implemented
-		return false;
+		if (firstThrow == 10)
+			return true;
+		else
+			return false;
 	}
 	
 	//return whether a frame is a spare or not
